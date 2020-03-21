b6e299b283c06f64dbe562b532dea4a0e596db6c
NitroFuN <theedge5670@gmail.com>
2020-03-21 20:26:02 +0000

    Revert "vince: Use tissot fingerprint"
    This reverts commit 8363ddba16cafd28a6d6b03fdc076e0ee8815573.

--- revengeos_vince.mk
+++ revengeos_vince.mk
@@ -37,9 +37,9 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.0.4.0.OEGMIFH release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V10.0.4.0.OEGMIFH:user/release-keys"
 
 TARGET_VENDOR := xiaomi
