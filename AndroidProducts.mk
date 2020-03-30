4cd9e39d394d06f841d9840674e9f63b91d75321
NitroFuN <theedge5670@gmail.com>
2020-03-30 22:26:32 +0000

    vince: SuperiorOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_vince.mk
+    $(LOCAL_DIR)/superior_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_vince-user \
-    revengeos_vince-userdebug \
-    revengeos_vince-eng
+    superior_vince-user \
+    superior_vince-userdebug \
+    superior_vince-eng
